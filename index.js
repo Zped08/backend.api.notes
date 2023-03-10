973c4915c45fa17b3aaad4a6b35ab2fbf0368c71
Jair Flores <jairflores0802@gmail.com>
2023-03-10 15:32:19 +0100

    api backend lista

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
+
+app.use((request, response, next) => {
+  console.log(request.method)
+  console.log(request.path)
+  console.log(request.body)
+  console.log('--------------')
+  next()
+})
 
 let notes = [
   {
